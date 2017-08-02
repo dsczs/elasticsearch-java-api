8f9ec0ebc1229b66b479ff68679e11b509df65b0
方磊 <felayman@163.com>
2017-08-02 14:58:13 +0800

    term and range query

---
+++ src/test/java/com/vcg/community/elasticsearch/query/TermAndRangeQueryDemo.java
@@ -0,0 +1,14 @@
+package com.vcg.community.elasticsearch.query;
+
+import com.vcg.community.elasticsearch.BaseDemo;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.junit.Test;
+
+/**
+ * @auhthor lei.fang@shijue.me
+ * @since . 2017-08-02
+ */
+public class TermAndRangeQueryDemo extends BaseDemo {
+
+
+}
