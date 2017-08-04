4887b63415b7c0a9c2a04c00f48592af99eab0e5
方磊 <felayman@163.com>
2017-08-04 13:03:38 +0800

    elasticsearch-restclient  basic demo

--- src/test/java/org/visualchina/elasticsearch/api/rest/RestQueryDemo.java
+++ src/test/java/org/visualchina/elasticsearch/api/rest/RestQueryDemo.java
@@ -1,10 +1,17 @@
 package org.visualchina.elasticsearch.api.rest;
 
-import org.apache.http.Header;
-import org.apache.http.message.BasicHeader;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import org.apache.http.HttpEntity;
+import org.apache.http.entity.ContentType;
+import org.apache.http.nio.entity.NStringEntity;
+import org.apache.http.util.EntityUtils;
 import org.elasticsearch.client.Response;
 import org.junit.Test;
 import org.visualchina.elasticsearch.api.demo.XPackBaseDemo;
+
+import java.util.Collections;
 
 /**
  * @auhthor lei.fang@shijue.me
@@ -17,5 +24,81 @@ public class RestQueryDemo extends XPackBaseDemo{
         String method = "GET";
         String endpoint = "/_cat";
         Response response = restClient.performRequest(method,endpoint);
+        System.out.println(EntityUtils.toString(response.getEntity()));
     }
+
+    @Test
+    public void testCreateIndex() throws Exception {
+        String method = "PUT";
+        String endpoint = "/test-index";
+        Response response = restClient.performRequest(method,endpoint);
+        System.out.println(EntityUtils.toString(response.getEntity()));
+    }
+
+    @Test
+    public void testCreateDocument() throws Exception {
+        String method = "PUT";
+        String endpoint = "/twitter/tweet/1";
+        HttpEntity entity = new NStringEntity(
+                "{\n" +
+                        "    \"user\" : \"kimchy\",\n" +
+                        "    \"post_date\" : \"2009-11-15T14:12:12\",\n" +
+                        "    \"message\" : \"trying out Elasticsearch\"\n" +
+                        "}", ContentType.APPLICATION_JSON);
+
+        Response response = restClient.performRequest(method,endpoint, Collections.<String, String>emptyMap(),entity);
+        System.out.println(EntityUtils.toString(response.getEntity()));
+    }
+
+    @Test
+    public void testGetDocument() throws Exception {
+        String method = "GET";
+        String endpoint = "/twitter/tweet/1";
+        Response response = restClient.performRequest(method,endpoint);
+        System.out.println(EntityUtils.toString(response.getEntity()));
+    }
+
+    @Test
+    public void testQueryAll() throws Exception {
+        String method = "POST";
+        String endpoint = "/twitter/tweet/_search";
+        HttpEntity entity = new NStringEntity("{\n" +
+                "  \"query\": {\n" +
+                "    \"match_all\": {}\n" +
+                "  }\n" +
+                "}", ContentType.APPLICATION_JSON);
+
+        Response response = restClient.performRequest(method,endpoint,Collections.<String, String>emptyMap(),entity);
+        System.out.println(EntityUtils.toString(response.getEntity()));
+    }
+
+    @Test
+    public void testQueryByField() throws Exception {
+        String method = "POST";
+        String endpoint = "/twitter/tweet/_search";
+        HttpEntity entity = new NStringEntity("{\n" +
+                "  \"query\": {\n" +
+                "    \"match\": {\n" +
+                "      \"user\": \"kimchy\"\n" +
+                "    }\n" +
+                "  }\n" +
+                "}", ContentType.APPLICATION_JSON);
+
+        Response response = restClient.performRequest(method,endpoint,Collections.<String, String>emptyMap(),entity);
+        System.out.println(EntityUtils.toString(response.getEntity()));
+    }
+
+    @Test
+    public void testUpdateByScript() throws Exception {
+        String method = "POST";
+        String endpoint = "/twitter/tweet/1/_update";
+        HttpEntity entity = new NStringEntity("{\n" +
+                "  \"doc\": {\n" +
+                "    \"user\":\"大美女\"\n" +
+                "  }\n" +
+                "}", ContentType.APPLICATION_JSON);
+        Response response = restClient.performRequest(method,endpoint,Collections.<String, String>emptyMap(),entity);
+        System.out.println(EntityUtils.toString(response.getEntity()));
+    }
+
 }
